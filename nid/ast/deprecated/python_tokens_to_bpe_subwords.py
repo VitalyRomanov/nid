a6c30198fec5b4b69b841e7aab7233f554a62079
Vitaly R <mortiv16@gmail.com>
2024-09-22 14:14:05 +0200

    removed deprecated files

--- nid/ast/deprecated/python_tokens_to_bpe_subwords.py
+++ nid/ast/deprecated/python_tokens_to_bpe_subwords.py
@@ -1,82 +1,82 @@
-# "▁"
-# TODO consider deprecating all of this
-from functools import lru_cache
-
-python_ops_to_bpe = {
-    'Add': ["▁+"],
-    'Mod': ["▁%"],
-    'NotIn': ["▁not", "▁in"],
-    'And': ["▁and"],
-    'IsNot': ["▁is", "▁not"],
-    'Or': ["▁or"],
-    'Is': ["▁is"],
-    'Eq': ["▁=="],
-    'Gt': ["▁>"],
-    'NotEq': ["▁!="],
-    'Not': ["▁not"],
-    'In': ["▁in"],
-    'BitAnd': ["▁&"],
-    'Sub': ["▁-"],
-    'Mult': ["▁*"],
-    'BitOr': ["▁|"],
-    'Lt': ["▁<"],
-    'Div': ["▁/"],
-    'LtE': ["▁<="],
-    'Pow': ["▁**"],
-    'FloorDiv': ["▁//"],
-    'GtE': ["▁>="],
-    'USub': ["▁-"],
-    'Invert': ["▁~"],
-    'UAdd': ["▁+"],
-    'MatMult': ["▁@"],
-    'BitXor': ["▁^"],
-    'LShift': ["▁<<"],
-    'RShift': ["▁>>"],
-    "Break": ["▁break"],
-    "Pass": ["▁pass"],
-    "Continue": ["▁continue"],
-}
-
-python_ops_to_literal = {
-    'Add': "+",
-    'Mod': "%",
-    'NotIn': "not in",
-    'And': "and",
-    'IsNot': "is not",
-    'Or': "or",
-    'Is': "is",
-    'Eq': "==",
-    'Gt': ">",
-    'NotEq': "!=",
-    'Not': "not",
-    'In': "in",
-    'BitAnd': "&",
-    'Sub': "-",
-    'Mult': "*",
-    'BitOr': "|",
-    'Lt': "<",
-    'Div': "/",
-    'LtE': "<=",
-    'Pow': "**",
-    'FloorDiv': "//",
-    'GtE': ">=",
-    'USub': "-", # this appears to be the operator to change number sign -5
-    'Invert': "~",
-    'UAdd': "+", # this appears to be the operator to change number sign +5, method __pos__
-    'MatMult': "@",
-    'BitXor': "^",
-    'LShift': "<<",
-    'RShift': ">>",
-    "Break": "break",
-    "Pass": "pass",
-    "Continue": "continue",
-}
-
-
-def op_tokenizer(op: str):
-    return python_ops_to_bpe.get(op, op)
-
-
-@lru_cache
-def op_tokenize_or_none(op, tokenize_func):
-    return tokenize_func(python_ops_to_literal[op]) if op in python_ops_to_literal else None
+# # "▁"
+# # TODO consider deprecating all of this
+# from functools import lru_cache
+#
+# python_ops_to_bpe = {
+#     'Add': ["▁+"],
+#     'Mod': ["▁%"],
+#     'NotIn': ["▁not", "▁in"],
+#     'And': ["▁and"],
+#     'IsNot': ["▁is", "▁not"],
+#     'Or': ["▁or"],
+#     'Is': ["▁is"],
+#     'Eq': ["▁=="],
+#     'Gt': ["▁>"],
+#     'NotEq': ["▁!="],
+#     'Not': ["▁not"],
+#     'In': ["▁in"],
+#     'BitAnd': ["▁&"],
+#     'Sub': ["▁-"],
+#     'Mult': ["▁*"],
+#     'BitOr': ["▁|"],
+#     'Lt': ["▁<"],
+#     'Div': ["▁/"],
+#     'LtE': ["▁<="],
+#     'Pow': ["▁**"],
+#     'FloorDiv': ["▁//"],
+#     'GtE': ["▁>="],
+#     'USub': ["▁-"],
+#     'Invert': ["▁~"],
+#     'UAdd': ["▁+"],
+#     'MatMult': ["▁@"],
+#     'BitXor': ["▁^"],
+#     'LShift': ["▁<<"],
+#     'RShift': ["▁>>"],
+#     "Break": ["▁break"],
+#     "Pass": ["▁pass"],
+#     "Continue": ["▁continue"],
+# }
+#
+# python_ops_to_literal = {
+#     'Add': "+",
+#     'Mod': "%",
+#     'NotIn': "not in",
+#     'And': "and",
+#     'IsNot': "is not",
+#     'Or': "or",
+#     'Is': "is",
+#     'Eq': "==",
+#     'Gt': ">",
+#     'NotEq': "!=",
+#     'Not': "not",
+#     'In': "in",
+#     'BitAnd': "&",
+#     'Sub': "-",
+#     'Mult': "*",
+#     'BitOr': "|",
+#     'Lt': "<",
+#     'Div': "/",
+#     'LtE': "<=",
+#     'Pow': "**",
+#     'FloorDiv': "//",
+#     'GtE': ">=",
+#     'USub': "-", # this appears to be the operator to change number sign -5
+#     'Invert': "~",
+#     'UAdd': "+", # this appears to be the operator to change number sign +5, method __pos__
+#     'MatMult': "@",
+#     'BitXor': "^",
+#     'LShift': "<<",
+#     'RShift': ">>",
+#     "Break": "break",
+#     "Pass": "pass",
+#     "Continue": "continue",
+# }
+#
+#
+# def op_tokenizer(op: str):
+#     return python_ops_to_bpe.get(op, op)
+#
+#
+# @lru_cache
+# def op_tokenize_or_none(op, tokenize_func):
+#     return tokenize_func(python_ops_to_literal[op]) if op in python_ops_to_literal else None
